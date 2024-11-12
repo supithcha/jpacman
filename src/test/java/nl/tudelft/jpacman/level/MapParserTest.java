a987faa77a19078c99b27fda391cc7186ab2bcc3
Supithcha Jongphoemwatthanaphon <pooppub3@gmail.com>
2024-11-12 12:53:59 +0700

    Exercise - JPacman 5

---
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -0,0 +1,77 @@
+package nl.tudelft.jpacman.level;
+//
+//public class MapParserTest {
+//}
+
+
+import nl.tudelft.jpacman.PacmanConfigurationException;
+import nl.tudelft.jpacman.board.BoardFactory;
+import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * This is a test class for MapParser.
+ */
+@ExtendWith(MockitoExtension.class)
+public class MapParserTest {
+    @Mock
+    private BoardFactory boardFactory;
+    @Mock
+    private LevelFactory levelFactory;
+    @Mock
+    private Blinky blinky;
+
+    /**
+     * Test for the parseMap method (good map).
+     */
+    @Test
+    public void testParseMapGood() {
+        MockitoAnnotations.initMocks(this);
+        assertNotNull(boardFactory);
+        assertNotNull(levelFactory);
+        Mockito.when(levelFactory.createGhost()).thenReturn(blinky);
+        MapParser mapParser = new MapParser(levelFactory, boardFactory);
+        ArrayList<String> map = new ArrayList<>();
+        map.add("############");
+        map.add("#P        G#");
+        map.add("############");
+        mapParser.parseMap(map);
+
+        verify(levelFactory, times(1)).createGhost();
+
+        // Verify that board elements are created correctly
+        verify(boardFactory, times(20)).createGround();
+        verify(boardFactory, times(22)).createWall();
+    }
+
+    /**
+     * Test for an invalid map with unsupported characters.
+     */
+    @Test
+    public void testParseMapInvalidCharacter() {
+        MapParser mapParser = new MapParser(levelFactory, boardFactory);
+        ArrayList<String> map = new ArrayList<>();
+        map.add("############");
+        map.add("#P    X   G#"); // 'X' is unsupported
+
+        assertThrows(PacmanConfigurationException.class, () -> {
+            mapParser.parseMap(map);
+        });
+    }
+
+
+}
+
+
+
